98c8616df3784c5e3b7f3c3a4865972eb5062673
adarshkunwar123 <adarsh.kunwar@wealthlane.co>
2025-10-16 14:11:56 +0545

    feat: run with yarn package manager

--- src/commands/init/react-starter.ts
+++ src/commands/init/react-starter.ts
@@ -14,19 +14,41 @@ const reactStarter = async ({
     process.exit(1);
   }
 
-  console.log(
-    "you have the projectAnswers installation method",
-    installation_method
-  );
+  switch (installation_method) {
+    case "npm":
+      const npmChild = spawn("npm", ["create", "vite@latest", name], {
+        shell: true,
+        stdio: "inherit", // This passes through the interactive prompts
+      });
 
-  const child = spawn("npm", ["create", "vite@latest", name], {
-    shell: true,
-    stdio: "inherit", // This passes through the interactive prompts
-  });
+      npmChild.stdout?.on("data", (data) => {
+        console.log(data.toString());
+      });
+      break;
+    case "yarn":
+      const yarnChild = spawn("yarn", ["create", "vite", name], {
+        shell: true,
+        stdio: "inherit", // This passes through the interactive prompts
+      });
 
-  child.stdout?.on("data", (data) => {
-    console.log(data.toString());
-  });
+      yarnChild.stdout?.on("data", (data) => {
+        console.log(data.toString());
+      });
+      break;
+    case "pnpm":
+      const pnpmChild = spawn("pnpm", ["create", "vite", name], {
+        shell: true,
+        stdio: "inherit", // This passes through the interactive prompts
+      });
+
+      pnpmChild.stdout?.on("data", (data) => {
+        console.log(data.toString());
+      });
+      break;
+    default:
+      console.error(`${installation_method} is not supported`);
+      process.exit(1);
+  }
 };
 
 export { reactStarter };
